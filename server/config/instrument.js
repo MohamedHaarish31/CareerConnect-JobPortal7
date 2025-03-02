98d74d99b145850f093bc3c27a21421810c2b1da
MohamedHaarish07 <mohamedhaarish55@gmail.com>
2025-03-02 13:13:46 +0530

    first commit

---
+++ server/config/instrument.js
@@ -0,0 +1,27 @@
+// Import with `"` if you are using ESM
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://5367a036e93144e82090d95301d3517f@o4508897572683776.ingest.us.sentry.io/4508897604861952",
+  integrations: [
+    nodeProfilingIntegration(),
+
+  ],
+  // Tracing
+ // tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
+// Manually call startProfiler and stopProfiler
+// to profile the code in between
+Sentry.profiler.startProfiler();
+integrations: Sentry.mongooseIntegration()
+// Starts a transaction that will also be profiled
+Sentry.startSpan({
+  name: "My First Transaction",
+}, () => {
+  // the code executing inside the transaction will be wrapped in a span and profiled
+});
+
+// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
+// your application until the process exits or stopProfiling is called.
+Sentry.profiler.stopProfiler();
